26f1833143c169ca0f6d68cb6fc313cbb89743cc
Alex Vieira Dias <alexvieiradias2019@gmail.com>
2023-08-21 00:29:38 -0300

    Update Uni3Exe11.java

--- unidade3/Uni3Exe11.java
+++ unidade3/Uni3Exe11.java
@@ -1,9 +1,8 @@
+import java.util.Scanner;
 /*
 Descreva um programa que dado uma temperatura em °C informe o seu valor em °F. Fórmula:
 °F = (9/5) °C + 32
 */
-
-import java.util.Scanner;
 
 public class Uni3Exe11 {
     public static void main(String[] args) throws Exception {
