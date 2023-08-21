5cb7ac171d4e9f14c52ac8eda351006fdd3566a3
Alex Vieira Dias <alexvieiradias2019@gmail.com>
2023-08-21 00:26:03 -0300

    Update Uni3Exe01.java

--- unidade3/Uni3Exe01.java
+++ unidade3/Uni3Exe01.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
-
-//Uma imobiliária vende apenas terrenos retangulares. 
-//Faça um programa para ler as dimensões de um terreno e depois exibir a área do terreno.
+/*
+Uma imobiliária vende apenas terrenos retangulares. 
+Faça um programa para ler as dimensões de um terreno e depois exibir a área do terreno.
+*/
 public class Uni3Exe01 {
     public static void main(String[] args) throws Exception {
         double height, width;
