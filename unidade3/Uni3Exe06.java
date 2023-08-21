c46adde1874e3fc60c685644a9294f3152fe1efb
Alex Vieira Dias <alexvieiradias2019@gmail.com>
2023-08-21 00:33:29 -0300

    Update Uni3Exe06.java

--- unidade3/Uni3Exe06.java
+++ unidade3/Uni3Exe06.java
@@ -1,10 +1,9 @@
+import java.util.Scanner;
 /*
 Um restaurante cobra R$ 25,00 por cada quilo de refeição. 
 Escreva um programa que leia o peso do prato montado pelo cliente (em quilos) 
 e imprima o valor a pagar. O peso do prato é de 750 gramas.
 */
-
-import java.util.Scanner;
 
 public class Uni3Exe06 {
     public static void main(String[] args) throws Exception {
