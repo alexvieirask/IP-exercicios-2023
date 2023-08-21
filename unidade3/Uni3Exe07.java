8ddf5af8284ca3444d9c1dde0eb73526383e6b96
Alex Vieira Dias <alexvieiradias2019@gmail.com>
2023-08-21 00:28:24 -0300

    Update Uni3Exe07.java

--- unidade3/Uni3Exe07.java
+++ unidade3/Uni3Exe07.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
-
-//Uma fábrica de refrigerantes vende seu produto em três formatos: lata de 350 ml, 
-//garrafa de 600 ml e garrafa de 2 litros. Se um comerciante compra uma determinada quantidade de cada formato, 
-//faça um programa para calcular quantos litros de refrigerante ele comprou.
+/*
+Uma fábrica de refrigerantes vende seu produto em três formatos: lata de 350 ml, 
+garrafa de 600 ml e garrafa de 2 litros. Se um comerciante compra uma determinada quantidade de cada formato, 
+faça um programa para calcular quantos litros de refrigerante ele comprou.
+*/
 public class Uni3Exe07 {
     public static void main(String[] args) throws Exception {
         final int canOfSodaValue = 350;
