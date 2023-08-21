7c966301d644177e833058b07343f1a155109963
Alex Vieira Dias <alexvieiradias2019@gmail.com>
2023-08-21 00:29:20 -0300

    Update Uni3Exe05.java

--- unidade3/Uni3Exe05.java
+++ unidade3/Uni3Exe05.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 /*
 No pé direito do frango há um anel com um chip de identificação; 
 no pé esquerdo são dois anéis para  indicar o tipo de alimento que ele deve consumir. 
@@ -7,6 +6,7 @@ no pé esquerdo são dois anéis para  indicar o tipo de alimento que ele deve c
 Sabendo que o anel com chip custa R$ 4,00 e o anel 
 de alimento custa R$ 3,50, faça um programa para calcular o gasto total da granja para marcar todos os seus frangos.
 */
+
 public class Uni3Exe05 {
     public static void main(String[] args) throws Exception {
         final double ringChipValue = 4;
