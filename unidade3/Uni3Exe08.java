ded7676baa8ada97533bbcfe99c39a2adb4a0764
Alex Vieira Dias <alexvieiradias2019@gmail.com>
2023-08-21 00:32:21 -0300

    Update Uni3Exe08.java

--- unidade3/Uni3Exe08.java
+++ unidade3/Uni3Exe08.java
@@ -1,10 +1,9 @@
+import java.util.Scanner;
 /*
 Uma pessoa foi até uma casa de câmbio trocar dólares por reais. Para isto ela entregou um valor em dólares para o atendente. 
 Considerando que o atendente tem a cotação do dólar, descreva um programa para calcular quantos reais o 
 atendente deve devolver para a pessoa.
 */
-
-import java.util.Scanner;
 
 public class Uni3Exe08 {
     public static void main(String[] args) throws Exception {
