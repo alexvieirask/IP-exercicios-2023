82bef9a782b1baafbfe12a2bb91b84e09efaf21d
Alex Vieira Dias <alexvieiradias2019@gmail.com>
2023-09-04 21:41:01 -0300

    Delete Uni4Exe14.java

--- Uni4Exe14.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Uni4Exe14 {
-    public static void main(String[] args) throws Exception {
-        Scanner input = new Scanner(System.in);
-        
-        input.close();
-    }
-}
